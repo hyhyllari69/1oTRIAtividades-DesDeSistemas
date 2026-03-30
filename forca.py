eaee81555eb465a4d3038e6ac96184ca1baf56fb
Hyllari <isadora.barbosa29@escola.pr.gov.br>
2026-03-30 09:41:06 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -3,38 +3,51 @@ palavras = ["rocket raccoon", "yondu", "gamora", "mantis", "groot"]
 palavra = random.choice(palavras)
 palavra = palavra.upper()
 
-letras_acertadas = []
-for letra in palavra
-    letras_acertadas.append("_")
-acertou = False
-enforcou = False
-limite_tentativas = len(palavra) + 6
-tentativa = 1
-
-def mostrar_letras_acertadas():
-    for letra in letras_acertadas:
-        print(letra, end=" ")
-
-print("Duvido você adivinhar a palavra secreta >:)")
-while(not acertou and not enforcou):
-    # mostar as letras acertadas
-    mostrar_letras_acertadas()
-
-    print("")
-    chute = input("Digite uma letra: ")
-    indice = 0
+    letras_acertadas = []
     for letra in palavra:
-        if chute.upper() == letra:
-            letras_acertadas[indice] = letra
-        indice = indice + 1
+        letras_acertadas.append("_")
 
-    if tentativa == limite_tentativas:
-        print("Você perdeu :( kkkkk\nA palavra era:", palavra)
-        enforcou = True
+    acertou = False
+    enforcou = False
+    limite_tentativas = len(palavra) + 6
+    tentativa = 1
 
-    if letras_acertadas.count("_") == 0:
-        print("Parabéns!!! Você acertou a palavra :))")
+    def mostrar_letras_acertadas():
+        for letra in letras_acertadas:
+            print(letra, end=" ")
+
+    print("Tente adivinhar a palavra secreta: ")
+    while(not acertou and not enforcou):
+        # mostrar as letras acertadas
         mostrar_letras_acertadas()
-        acertou = True
+        print("")
+        chute = input("Digite uma letra: ")
+        indice = 0
+        for letra in palavra:
+            if chute.upper() == letra:
+                letras_acertadas[indice] = letra
+            indice = indice + 1
+        
+        if tentativa == limite_tentativas:
+            print("Você perdeu :(\nA palavra era: ", palavra)
+            enforcou = True
 
-    tentativa = tentativa + 1
+        if letras_acertadas.count("_") == 0:
+            print("Parabéns, você acertou a palavra secreta!")
+            mostrar_letras_acertadas()
+            acertou = True
+
+        tentativa = tentativa + 1
+
+
+    # TABELA VERDADE
+    # Quero fazer um suco de banana OU maça
+    # banana = true
+    # maçã = true
+    #   E       |       OU
+    # v v = v   |     v v = v
+    # V F = F   |     V F = V
+    # F V = F   |     F V = V
+    # F F = F   |     F F = F
+if(__name__ == "__main__"):
+    jogar()
